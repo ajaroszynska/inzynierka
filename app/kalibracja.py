e8891bcce546851bfdaad49e404bf49c98ea284f
ajaroszynska <a.jaroszynska@outlook.com>
2025-10-06 10:38:33 +0200

    Nie uzywaj wczesniejszego commitagit add . cos sie zwalilo. Tutaj kalibracja dziala, naproawiony pieciokat, nie dziala dla niebieskiego (tlo i szumy), szukana jedna wartosc thresh_ksztalt

--- app/kalibracja.py
+++ app/kalibracja.py
@@ -14,7 +14,7 @@ sciezka_zapisu = "./test_img/cal_img/" # placeholder - w linuksie inaczej jest
 # zdjecia_kalibracja = zapisz_n_zdjec(n_zdjec, sciezka_zapisu, "dist_cal", 0)
 # zdjecia_kalibracja = ['D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_0.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_1.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_2.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_3.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_4.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_5.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_6.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_7.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_8.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_9.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_10.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_11.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_12.jpg', 'D:\\studia\\inzynierka\\app\\test_img\\cal_img\\cal_test_13.jpg']
 zdjecia_kalibracja = glob.glob(sciezka_zapisu + "dist_cal*.jpg")
-print(zdjecia_kalibracja)
+# print(zdjecia_kalibracja)
 # mtx, dist, new_cam_mtx, roi = wsp_znieksztalcenia(zdjecia_kalibracja, sciezka_zapisu)
 
 # undistort
@@ -33,12 +33,25 @@ print(zdjecia_kalibracja)
 
 # Ustal wartość progową (threshold value) dla maskowania
 # Wybór liczby obiektów kalibracji
-thresh_ksztalt = 255
 
 n_obiekt = int(input("Liczba obiektow kalibracji: "))
+zdj = glob.glob(sciezka_zapisu + "object_*.jpg")
+# zdj = glob.glob(".\\test_img\\pieciokat_test*.jpg")
+# Pięciokąty:
+# zdj = [".\\test_img\\cal_img\\object_1.jpg", ".\\test_img\\cal_img\\object_3.jpg", ".\\test_img\\cal_img\\object_6.jpg"]
+i = 0
+thresh_ksztalt_wyniki = []
+
+cv2.namedWindow("Kalibracja", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Kalibracja", 668, 500)
+
+# Zmienne do wyswietlania obrazu
+# kolor = (255, 0, 0)
+# font = cv2.FONT_HERSHEY_DUPLEX
 
 # Wybór kształtu i koloru kolejnego obiektu 
 while n_obiekt:
+    thresh_ksztalt = 255
     spr_wybor = 1
     # Wybor ksztaltu
     while(spr_wybor):
@@ -76,29 +89,55 @@ while n_obiekt:
     # Umieszczenie obiektu przed kamerą i uchwycenie jego zdjęcia
     print("Umieść obiekt przed kamerą i wykonaj zdjęcie")
     # zdj = zapisz_n_zdjec(1, sciezka_zapisu, "obiekt", 1)
-    zdj = glob.glob(sciezka_zapisu + 'obiekt*.jpg')
-    print(zdj)
+    # zdj = glob.glob(sciezka_zapisu + 'obiekt*.jpg')
+    # zdj = [".\\test_img\\cal_img\\object_8.jpg"]
+    # print(zdj)
     # zdj = "D:\\studia\\inzynierka\\app\\test_img\\cal_img\\obiekt0.jpg"
     # Szukaj kształtu z różnymi wartościami thresh
-    thresh_ksztalt_temp = thresh_ksztalt
+    # thresh_ksztalt_temp = thresh_ksztalt
 
-    check, ksztalt, coords = detekcja_ksztaltu(zdj[0], thresh_ksztalt_temp)
+    while True:
+        thresh_ksztalt -= 1
+        if thresh_ksztalt <= 0:
+            break
 
-    while (not check and ksztalt != ksztalt_wybor) or thresh_ksztalt_temp <= 255:
-        thresh_ksztalt_temp -= 1
-        check, ksztalt, coords = detekcja_ksztaltu(zdj[0], thresh_ksztalt_temp)
+        check, ksztalt, coords, zdj_ksztalt, tekst = detekcja_ksztaltu(zdj[i], thresh_ksztalt)
         print('ksztalt: ' + str(ksztalt))
         print('ksztalt_wybor: ' + str(ksztalt_wybor))
         print('check: ' + str(check))
-        print('thresh: ' + str(thresh_ksztalt_temp))
-    thresh_ksztalt = thresh_ksztalt_temp
-    print(thresh_ksztalt)
+        print('thresh: ' + str(thresh_ksztalt))
+
+        cv2.imshow("Kalibracja", zdj_ksztalt)
+        cv2.waitKey(100)
+
+        if check and (ksztalt == ksztalt_wybor):
+            # cv2.putText(zdj_ksztalt, tekst, coords, font, 1, (0, 255, 255), 1)
+            
+            spr = input("Czy widoczny kształt? [t/n]: ")
+            match spr:
+                case 't':
+                    break
+                case 'n':
+                    thresh_ksztalt -= 5
+                    continue
+    thresh_ksztalt_wyniki.append(thresh_ksztalt)
+
+    # while ksztalt != ksztalt_wybor and thresh_ksztalt_temp >= 0:
+    #     thresh_ksztalt_temp -= 1
+    #     check, ksztalt, coords = detekcja_ksztaltu(zdj[i], thresh_ksztalt_temp)
+    #     print('ksztalt: ' + str(ksztalt))
+    #     print('ksztalt_wybor: ' + str(ksztalt_wybor))
+    #     print('check: ' + str(check))
+    #     print('thresh: ' + str(thresh_ksztalt_temp))
+    # thresh_ksztalt = thresh_ksztalt_temp
+    print(thresh_ksztalt_wyniki)
     # Szukaj koloru z różnymi wartościami thresh
 
 
     # Zapisz znalezione wartości
 
     n_obiekt -= 1
+    i += 1
     print(n_obiekt)
     print('end of iteration')
 
