3f1da49a73bdfe0348af1dbbfd3ce5acc83d73d7
ajaroszynska <a.jaroszynska@outlook.com>
2025-10-06 10:26:51 +0200

    Revert "kalibracja ksztaltu praktycznie dziala, dla niebeiskiego nie, wyszukiwanie jednej wartosci thresh_ksztalt"
    This reverts commit d59d1db2692baaf788d24f5fb8a15b2069aea5ee.

--- app/detekcja_ksztaltu.py
+++ app/detekcja_ksztaltu.py
@@ -7,9 +7,9 @@ def detekcja_ksztaltu(zdj_sciezka, thresh ):
 
     # Konwersja do obrazu czarno białego bez skali szarości
     _, zdj_thresh = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)
-    # resize = cv2.resize(zdj_thresh, (267, 200))
-    # cv2.imshow("zdj thresh", resize)
-    # cv2.waitKey(100)
+    resize = cv2.resize(zdj_thresh, (267, 200))
+    cv2.imshow("zdj thresh", resize)
+    cv2.waitKey(100)
 
     # Szukanie konturów i detekcja największego kształtu
     kontury, hierarchia = cv2.findContours(zdj_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -26,21 +26,16 @@ def detekcja_ksztaltu(zdj_sciezka, thresh ):
         check = False
         ksztalt = 0
         coords = 0
-        tekst = "brak"
-        return check, ksztalt, coords, zdj_thresh, tekst
+        return check, ksztalt, coords
     
     print("Najwiekszy ksztalt ma obszar: pola[{}] = {}".format(obj_index, obj_pole))
 
     # Przyblizenie ksztaltu aby wyeliminować szumy i niedokładności
     obj_kontur = kontury[obj_index]
 
-    eps = 0.05 * cv2.arcLength(obj_kontur, True)
+    eps = 0.1 * cv2.arcLength(obj_kontur, True)
     approx = cv2.approxPolyDP(obj_kontur, eps, True)
-    # cv2.drawContours(zdj_thresh, obj_kontur, -1, (255,0,0), 8)
-
-    # r = cv2.resize(zdj_thresh, (267,200))
-    # cv2.imshow('test', r)
-    # cv2.waitKey(500)
+    cv2.drawContours(zdj, obj_kontur, -1, (255,0,0), 8)
 
     # Wyznacz wspołrzędne obiektu
     x, y, w, h = cv2.boundingRect(approx)
@@ -48,33 +43,30 @@ def detekcja_ksztaltu(zdj_sciezka, thresh ):
     y_mid = int(y + (h/1.5))
     coords = (x_mid, y_mid)
 
+    # Zmienne do wyswietlania obrazu
+    kolor = (0, 0, 0)
+    font = cv2.FONT_HERSHEY_DUPLEX
 
     if len(approx) == 3:
         # Trójkąt
         ksztalt = 3
         check = True
-        tekst = "Trojkat"
+        cv2.putText(zdj, "Trójkąt", coords, font, 1, kolor, 1)
     elif len(approx) == 4:
         # Prostokąt
         ksztalt = 4
         check = True
-        tekst = "Prostokat"
+        cv2.putText(zdj, "Prostokąt", coords, font, 1, kolor, 1)
+
     elif len(approx) == 5:
         # Pięciokąt
         ksztalt = 5
         check = True
-        tekst = 'Pieciokat'
+        cv2.putText(zdj, "Pięciokąt", coords, font, 1, kolor, 1)
     else:
         # Brak
         ksztalt = 0
         check = False
-        tekst = "Brak"
-    
-    cv2.imwrite(".\\test_img\\cal_img\\" + "temp.png", zdj_thresh)
-    zdj_wynik = cv2.imread(".\\test_img\\cal_img\\" + "temp.png")
-    kolor = (0, 255, 0)
-    font = cv2.FONT_HERSHEY_DUPLEX
-    zdj_wynik = cv2.rectangle(zdj_wynik, (x, y), (x + w, y + h), kolor, 3)
-    cv2.putText(zdj_wynik, tekst, (x + w, y + h), font, 1, kolor, 3)
+        cv2.putText(zdj, "Brak", coords, font, 1, kolor, 1)
 
-    return check, ksztalt, coords, zdj_wynik, tekst
+    return check, ksztalt, coords
